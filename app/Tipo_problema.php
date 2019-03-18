9c07cce01521ef9af4d833e2d9e21a5d01599c45
Erickson <erickson.rinho@hotmail.com>
2019-03-18 17:51:38 -0300

    #1 - Alterando a migration e a model da tabela Tipo_problemas

--- app/Tipo_problema.php
+++ app/Tipo_problema.php
@@ -3,8 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Tipo_problema extends Model
 {
-    //
+    /*nome da tabela*/
+	protected $table 	= 	"tipo_problemas";
+
+	/*nome dos atributos que poderão ser não alterados*/
+	protected $guarded	= ['probl_id'];
+
+	/*nome dos atributos que poderão ser alterados*/
+	protected $fillable = ['probl_tipo'];
+
+	/*Função que representa o relacionamento de muitos para um*/
+	  public function cham_tipo_probl(){
+         return $this->belongsTo(Chamado::class);
+     }	
 }
