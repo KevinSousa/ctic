6d0eb194402233eb871f8c7d4e43e0a2b705e84a
Erickson <erickson.rinho@hotmail.com>
2019-03-18 14:33:38 -0300

    #1 - Adicionando a model e a migration das Salas

---
+++ database/migrations/2019_03_18_164434_create_salas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSalasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('salas', function (Blueprint $table) {
+            $table->increments('sala_id');
+            $table->string('sala_identificacao');
+            $table->enum('sala_andar', ['BLOCO A','BLOCO B', 'BLOCO C']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('salas');
+    }
+}
