7976861a7b53528582156de2939e155b995dc9a1
Erickson <erickson.rinho@hotmail.com>
2019-03-18 17:52:26 -0300

    #1 - Alterando a model Funcao

--- app/Funcao.php
+++ app/Funcao.php
@@ -2,9 +2,23 @@
 
 namespace App;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Funcao extends Model
 {
-    //
+    /*nome da tabela*/
+	protected $table 	= 	"funcaos";
+
+	/*nome dos atributos que poderão ser não alterados*/
+	protected $guarded	= ['funcao_id'];
+
+	/*nome dos atributos que poderão ser alterados*/
+	protected $fillable = ['funcao_nome'];
+		
+	/*Função que representa o relacionamento de muitos para um*/
+	 public function func_funcao(){
+         return $this->belongsTo(Funcionario::class);
+     }
 }
