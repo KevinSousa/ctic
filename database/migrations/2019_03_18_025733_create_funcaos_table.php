d87f5e67ac115ae99201d58b75251b6313c8ffb8
Erickson <erickson.rinho@hotmail.com>
2019-03-18 00:01:37 -0300

    #1 - Começando a colocar o banco no Laravel

---
+++ database/migrations/2019_03_18_025733_create_funcaos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Funcoes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('funcaos', function (Blueprint $table) {
+            $table->increments('funcao_id');
+            $table->string('funcao_name');
+        });    
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('funcaos');
+    }
+}
