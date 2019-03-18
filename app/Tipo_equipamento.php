6e720276daeb56efee31eb90c338718e5bf06c4d
Erickson <erickson.rinho@hotmail.com>
2019-03-18 17:53:11 -0300

    #1 - Alterando a model Tipo_equipamento

--- app/Tipo_equipamento.php
+++ app/Tipo_equipamento.php
@@ -3,8 +3,26 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Tipo_equipamento extends Model
 {
-    //
+    /*nome da tabela*/
+	protected $table 	= 	"tipo_equipamentos";
+
+	/*nome dos atributos que poderão ser não alterados*/
+	protected $guarded	= ['tipo_id'];
+
+	/*nome dos atributos que poderão ser alterados*/
+	protected $fillable = ['tipo_nome'];
+	
+	/*Função que representa o relacionamento de muitos para um*/
+	  public function equip_tipo_equip(){
+        return $this->belongsTo(Equipamento::class);
+ 	}  	
+
+ 	/*Função que representa o relacionamento de muitos para um*/
+	  public function cham_tipo_probl(){
+         return $this->belongsTo(Chamado::class);
+     }
 }
