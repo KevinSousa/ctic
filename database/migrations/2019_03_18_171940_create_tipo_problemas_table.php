19442baef6772ef3b0806b0825addfb5c9a0c8d4
Erickson <erickson.rinho@hotmail.com>
2019-03-18 14:34:35 -0300

    #1 - Adicionando a model e a migration dos Tipos de problemas

---
+++ database/migrations/2019_03_18_171940_create_tipo_problemas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTipoProblemasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tipo_problemas', function (Blueprint $table) {
+            $table->inccrements(' probl_id');
+            $table->string(' probl_tipo');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tipo_problemas');
+    }
+}
