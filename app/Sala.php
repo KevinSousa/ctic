e74a5a16711e1cf49dec73ff719b1cc110024590
Erickson <erickson.rinho@hotmail.com>
2019-03-18 17:52:41 -0300

    #1 - Alterando a model Sala

--- app/Sala.php
+++ app/Sala.php
@@ -3,8 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Sala extends Model
 {
-    //
+	/*nome da tabela*/
+	protected $table 	= 	"salas";
+
+	/*nome dos atributos que poderão ser não alterados*/
+	protected $guarded	= ['cham_id','cham_funcionario','cham_sala', 'cham_equip', 'champ_tipo_problema'];
+
+	/*nome dos atributos que poderão ser alterados*/
+	protected $fillable = ['cham_grau_urgencia', 'cham_descricao'];
+
+	/*nome dos atributos que representam as horas*/
+	protected $date 	= ['cham_data_chamado', 'cham_data_prevista'];
+		
+	/*Função que representa o relacionamento de muitos para um*/
+	 public function cham_sala(){
+         return $this->belongsTo(Chamado::class);
+     } 
 }
