336a584358b2fd45394772f767393bb0ac3f3848
KevinSousa <kevinsmoura@hotmail.com>
2019-03-20 22:07:48 -0300

    #3 Criando algumas rotas para o CRUD

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> CTIC </title>
+</head>
+<body>
+<h1> Chamados CTIC </h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,18 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+// Route::get('/', ['as' => 'home' , 'uses' => 'UsersController@index']);
+
+Route::get('/remover/{id}', ['as' => 'remover', 'uses' => 'Controller@remover']);
+
+Route::post('/salvar', ['as' => 'salvar', 'uses' => 'Controller@salvar']);
+
+Route::get('/editar/{id}', ['as' => 'editar', 'uses' => 'Controller@editar']);
+
+Route::put('/atualizar/{id}',['as'=>'atualizar','uses'=>'Controller@atualizar']);
+
+
+
