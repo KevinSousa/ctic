2c4d38d83876f94bbe27e32cbddddcf1b58f5170
Erickson <erickson.rinho@hotmail.com>
2019-03-29 00:48:34 -0300

    #4 - Removendo varios links de Css e Js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,119 +10,54 @@
     <meta name="keywords" content="au theme template">
 
     <!-- Title Page-->
-    <title>Dashboarsad</title>
+    <title>CTIC</title>
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" />
 
-    <!-- Fontfaces CSS-->
-    <link href="{{ asset('css/font-face.css') }}" rel="stylesheet" media="all">
+        <!-- Fontfaces CSS-->
+    <link href="{{ asset('css/font-face.css') }}') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('vendor/font-awesome-4.7/css/font-awesome.min.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('vendor/font-awesome-5/css/fontawesome-all.min.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('vendor/mdi-font/css/material-design-iconic-font.min.css') }}" rel="stylesheet" media="all">
+    <!-- Bootstrap CSS-->
+    <link href="{{ asset('vendor/bootstrap-4.1/bootstrap.min.css') }}" rel="stylesheet" media="all">
+    <!-- Vendor CSS-->
+    <link href="{{ asset('vendor/animsition/animsition.min.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('vendor/wow/animate.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('vendor/css-hamburgers/hamburgers.min.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('vendor/slick/slick.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('vendor/select2/select2.min.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('vendor/perfect-scrollbar/perfect-scrollbar.css') }}" rel="stylesheet" media="all">
 
-     <!-- Main CSS-->
+    <!-- Main CSS-->
     <link href="{{ asset('css/theme.css') }}" rel="stylesheet" media="all">
 
-    <!-- JS-->
-    <link href="{{ asset('js/main.js') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/jquery-3.2.1.min.js') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/jquery-ui.min.js') }}" rel="stylesheet" media="all">
-   
-    <!-- Animsition-->
-    <link href="{{ asset('vendor/Animsition/animsition.min.css') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/Animsition/animsition.min.js') }}" rel="stylesheet" media="all">
     
-    <!-- Bootstrap_4.1-->
-    <link href="{{ asset('vendor/bootstrap-4.1/bootstrap.min.css') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/bootstrap-4.1/bootstrap.min.js') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/bootstrap-4.1/popper.min.css') }}" rel="stylesheet" media="all">
 
-    <!-- Bootstrap-progressbar-->
-    <link href="{{ asset('vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/bootstrap-progressbar/bootstrap-progressbar.min.css') }}" rel="stylesheet" media="all">
+    <!-- Jquery JS-->
+    <script src="{{ asset('vendor/jquery-3.2.1.min.js') }}"></script>
+    <!-- Bootstrap JS-->
+    <script src="{{ asset('vendor/bootstrap-4.1/popper.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap-4.1/bootstrap.min.js') }}"></script>
+    <!-- Vendor JS       -->
+    <script src="{{ asset('vendor/slick/slick.min.js') }}">
+    </script>
+    <script src="{{ asset('vendor/wow/wow.min.js') }}"></script>
+    <script src="{{ asset('vendor/animsition/animsition.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap-progressbar/bootstrap-progressbar.min.js') }}">
+    </script>
+    <script src="{{ asset('vendor/counter-up/jquery.waypoints.min.js') }}"></script>
+    <script src="{{ asset('vendor/counter-up/jquery.counterup.min.js') }}">
+    </script>
+    <script src="{{ asset('vendor/circle-progress/circle-progress.min.js') }}"></script>
+    <script src="{{ asset('vendor/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+    <script src="{{ asset('vendor/chartjs/Chart.bundle.min.js') }}"></script>
+    <script src="{{ asset('vendor/select2/select2.min.js') }}">
+    </script>
 
-		<!-- Circle-progress-->
-    <link href="{{ asset('vendor/circle-progressbar/circle-progressbar.js') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/circle-progressbar/circle-progressbar.min.js') }}" rel="stylesheet" media="all">		
+    <!-- Main JS-->
+    <script src="js/main.js"></script>
 
-    <!-- Countdown-->
-    <link href="{{ asset('vendor/countdown/countdowntime.js') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/countdown/countdowntime.min.js') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/countdown/moment-timezone-with-data.min.js') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/countdown/moment-timezone.min.js') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/countdown/moment.min.js') }}" rel="stylesheet" media="all">
-
-    <!-- Counter-up-->
-    <link href="{{ asset('vendor/counter-up/jquery.counterup.min.js') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/counter-up/jquery.waypoints.min.js') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/counter-up/waypoints.min.js') }}" rel="stylesheet" media="all"> 
-
-    <!-- Css-Hamburguer-->
-    <link href="{{ asset('vendor/css-hamburgers/hamburgers.css') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/css-hamburgers/hamburgers.min.css') }}" rel="stylesheet" media="all">
-
-    <!-- Font-awesome-4.7-->
-    <link href="{{ asset('vendor/font-awesome-4.7/css/font-awesome.css') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/font-awesome-4.7/css/font-awesome.min.css') }}" rel="stylesheet" media="all">
-
- 		<!-- Font-awesome-5-->
-    <link href="{{ asset('vendor/font-awesome-5/css/fontawesome-all.min.css') }}" rel="stylesheet" media="all">
-
-		<!-- Lightbox2-->
-    <link href="{{ asset('vendor/lightbox2/dist/css/lightbox.css') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/lightbox2/dist/js/lightbox-plus-jquery.js') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/lightbox2/dist/js/lightbox-plus-jquery.min.js') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/lightbox2/dist/js/lightbox-plus-jquery.min.map') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/lightbox2/dist/js/lightbox.js') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/lightbox2/dist/js/lightbox.min.js') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/lightbox2/dist/js/lightbox.min.map') }}" rel="stylesheet" media="all">
-
-		 <!-- Mdi-font-->
-    <link href="{{ asset('vendor/mdi-font/css/material-design-iconic-font.css') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/mdi-font/css/material-design-iconic-font.min.css') }}" rel="stylesheet" media="all">
-
-		 <!-- Perfect-scrollbar-->
-    <link href="{{ asset('vendor/perfect-scrollbar/perfect-scrollbar.css') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/perfect-scrollbar/perfect-scrollbar.js') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/perfect-scrollbar/perfect-scrollbar.min.css') }}" rel="stylesheet" media="all">
-
-		 <!-- Progressbar-->
-    <link href="{{ asset('vendor/progressbar/progressbar.js') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/progressbar/progressbar.min.js') }}" rel="stylesheet" media="all">
-		
-		<!-- Select2-->
-    <link href="{{ asset('vendor/select2/select2.min.css') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/select2/select2.min.js') }}" rel="stylesheet" media="all">
-
-    <!-- Slick-->
-    <link href="{{ asset('vendor/slick/slick-theme.css') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/slick/slick-theme.less') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/slick/slick-theme.scss') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/slick/slick.css') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/slick/slick.js') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/slick/slick.less') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/slick/slick.min.js') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/slick/slick.scss') }}" rel="stylesheet" media="all">
-
-    <!-- Smk-accordion-->
-    <link href="{{ asset('vendor/smk-accordion/accordion.css') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/smk-accordion/accordion.js') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/smk-accordion/accordion.min.js') }}" rel="stylesheet" media="all">
-
-		<!-- Sweetalert-->
-    <link href="{{ asset('vendor/sweetalert/sweetalert.min.js') }}" rel="stylesheet" media="all">		
-
-    <!-- Vector-map-->
-    <link href="{{ asset('vendor/vector-map/jquery.vmap.brazil.js') }}" rel="stylesheet" media="all">		
-    <link href="{{ asset('vendor/vector-map/jquery.vmap.europe.js') }}" rel="stylesheet" media="all">		
-    <link href="{{ asset('vendor/vector-map/jquery.vmap.france.js') }}" rel="stylesheet" media="all">		
-    <link href="{{ asset('vendor/vector-map/jquery.vmap.germany.js') }}" rel="stylesheet" media="all">		
-    <link href="{{ asset('vendor/vector-map/jquery.vmap.js') }}" rel="stylesheet" media="all">		
-    <link href="{{ asset('vendor/vector-map/jquery.vmap.min.js') }}" rel="stylesheet" media="all">		
-    <link href="{{ asset('vendor/vector-map/jquery.vmap.russia.js') }}" rel="stylesheet" media="all">		
-    <link href="{{ asset('vendor/vector-map/jquery.vmap.sampledata.js') }}" rel="stylesheet" media="all">		
-    <link href="{{ asset('vendor/vector-map/jquery.vmap.usa.js') }}" rel="stylesheet" media="all">		
-    <link href="{{ asset('vendor/vector-map/jquery.vmap.world.js') }}" rel="stylesheet" media="all">		
-    <link href="{{ asset('vendor/vector-map/jqvmap.min.css') }}" rel="stylesheet" media="all">		
-
-    <!-- Wow-->
-    <link href="{{ asset('vendor/animate.css') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/wow.min.js') }}" rel="stylesheet" media="all">
 </head>
 
 <body class="animsition">
