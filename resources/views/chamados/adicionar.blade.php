c4023d81638d60e4a6dbd7da3aec7664b44bae41
jhonnyfreitas1 <jhonnyfarias87@gmail.com>
2019-03-29 12:37:35 -0300

    #24 criando view de adicionar ainda faltando algumas modificações

--- resources/views/chamados/adicionar.blade.php
+++ resources/views/chamados/adicionar.blade.php
@@ -14,13 +14,37 @@
 <body>
     <div class="ui container">
         <h1>Cadastre seu chamado</h1>
+       
         <div>
-            <form action="{{ route('sala.salvar') }}" method="POST" class="ui form">
+            <form action="{{ route('chamados.salvar') }}" method="POST" class="ui form">
                 {{ csrf_field() }}
                 <div class="field">
-                    <label>Identificação da Sala</label>
-                    <input type="text" name="sala_identificacao" placeholder="">
+                    <label>IDescrição do problema</label>
+                    <input type="text" name="cham_descricao" placeholder="Ex:: Problema com o Computador que se desliga">
+                     <input type="hidden"  class="cham_data_chamado" value="{{date('Y-m-d H:i:s')}}">
                 </div>
+                     <label><b>Grau de urgencia<b></label>
+                    <select name="cham_grau_urgencia" class="ui fluid dropdown">
+                        <option></option>                    
+                        <option value="BAIXO">BAIXO</option>
+                        <option value="MÉDIO">MÉDIO</option>
+                        <option value="ALTA">ALTA</option>
+                    </select>
+
+                    <div class="field">
+                         <label><b>Sala<b></label>
+                    <select name="cham_sala" class="ui fluid dropdown">
+                        <option></option>   
+                        @foreach ($salas as $sala)
+                        <option value="{{$sala -> sala_id}}"> {{$sala -> sala_identificacao}}</option>
+                         @endforeach
+                    </select>
+                    </div>
+                    <div class="field">
+                        <label><b>Número de tombamento do equipamento<b></label>
+                        <input type="text" name="cham_equip" placeholder="Ex:: 5151551529">
+                    </div>
+
                 <div class="field">
                     <label>Andar/Bloco</label>
                     <select name="sala_andar" class="ui fluid dropdown">
@@ -30,9 +54,12 @@
                         <option value="BLOCO C">BLOCO C</option>
                     </select>
                 </div>        
-                <button class="ui button blue" type="submit">Adicionar</button>
+                <button class="ui button green" type="submit">Adicionar</button>
             </form>
-        </div>    
+              <a href="{{route('chamados.index')}}" style="float: right; margin-right:5em;"><i class="ui button blue"> Voltar</i></a> 
+        </div>  
+
     </div>    
+
 </body>
 </html>
