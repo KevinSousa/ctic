b2903a66777c8cba2b0bcdbe30389914a21013b3
Kevin Moura <kevinsmoura@hotmail.com>
2019-03-30 18:21:52 -0300

    #4 Melhorando visual do index chamado

--- resources/views/chamados/index.blade.php
+++ resources/views/chamados/index.blade.php
@@ -1,37 +1,19 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title> CTIC - Chamados </title>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.css">
-</head>
-<style type="text/css">
-	@import url('https://fonts.googleapis.com/css?family=Acme');
-	*{
-		font-family: 'Acme', sans-serif;
-	}
-	body {
-		margin-top: 25px;
-		margin-bottom: 30px;
-	}
-</style>
-<body>		
-	
-
-
-	<div class="ui container">
-		<h1>Chamados para Técnico</h1>
-		<h2> Lista de chamados</h2>
-		<div>
-			<a  class="ui primary button " href="{{route('chamados.add')}}">Faça um chamado </a>
-		
-				<table class="ui celled table">
+@extends('layouts.app')
+@section('content')	
+	<div id="index">
+		<div align="left">
+			<h2> Lista de chamados</h2>
+			<br>
+			<a href="{{route('chamados.add')}}"><button class="btn btn-primary"> Abrir Chamado </button></a>
+		</div><br>
+		<table class="ui celled table">
 			<tr align="center">
-				<th>Autor</th>
-				<th> Grau de urgencia</th>
-				<th>Onde encontrar  </th>
+				<th> Autor </th>
+				<th> Grau de Urgência </th>
+				<th> Onde encontrar </th>
 				<th> Descrição </th>
-				<th>tipo problema  </th>
-				<th>Ações</th>
+				<th> Tipo do Problema </th>
+				<th> Ações </th>
 				
 			</tr>
 
@@ -52,10 +34,11 @@
 				</tr>
 			@endforeach
 		</table>
-		</div>
-		
-		
-		<br>
 	</div>
-</body>
-</html>
+@endsection
+
+<style type="text/css">
+	div#index{
+		margin: 0px 25px 0px 25px;
+	}
+</style>
