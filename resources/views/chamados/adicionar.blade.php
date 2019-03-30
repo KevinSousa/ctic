5e3855aa53ada3929c00e42d53d7af81553b444b
jhonnyfreitas1 <jhonnyfarias87@gmail.com>
2019-03-30 14:27:25 -0300

    criando view para adicionar chamados

--- resources/views/chamados/adicionar.blade.php
+++ resources/views/chamados/adicionar.blade.php
@@ -21,7 +21,7 @@
                 <div class="field">
                     <label>IDescrição do problema</label>
                     <input type="text" name="cham_descricao" placeholder="Ex:: Problema com o Computador que se desliga">
-                     <input type="hidden"  class="cham_data_chamado" value="{{date('Y-m-d H:i:s')}}">
+                     <input type="hidden"  name="cham_data_chamado"  value="{{date('Y-m-d H:i:s')}}">
                 </div>
                      <label><b>Grau de urgencia<b></label>
                     <select name="cham_grau_urgencia" class="ui fluid dropdown">
@@ -30,19 +30,28 @@
                         <option value="MÉDIO">MÉDIO</option>
                         <option value="ALTA">ALTA</option>
                     </select>
-
                     <div class="field">
-                         <label><b>Sala<b></label>
-                    <select name="cham_sala" class="ui fluid dropdown">
-                        <option></option>   
-                        @foreach ($salas as $sala)
-                        <option value="{{$sala -> sala_id}}"> {{$sala -> sala_identificacao}}</option>
+                    <label><b>Categoria do problema<b></label>
+                    <select name="cham_tipo_problema" class="ui fluid dropdown">
+                         <option></option> 
+                        @foreach ($tipos_problemas as $tipo)
+                        <option value="{{$tipo -> probl_id}}">{{$tipo -> probl_tipo}}</option>
                          @endforeach
                     </select>
-                    </div>
+                    </div>  
+                       
                     <div class="field">
                         <label><b>Número de tombamento do equipamento<b></label>
                         <input type="text" name="cham_equip" placeholder="Ex:: 5151551529">
+                    </div>
+                    <div class="field">
+                         <label><b>Tipo de Equipamento<b></label>
+                    <select name="cham_equip" class="ui fluid dropdown">
+                          <option></option> 
+                        @foreach ($tipos_equip as $equip)
+                        <option value="{{$equip -> tipo_id}}"> {{$equip -> tipo_nome}}</option>
+                         @endforeach
+                    </select>
                     </div>
 
                 <div class="field">
@@ -53,12 +62,26 @@
                         <option value="BLOCO B">BLOCO B</option>
                         <option value="BLOCO C">BLOCO C</option>
                     </select>
-                </div>        
+                     <div class="field">
+                         <label><b>Sala<b></label>
+                    <select name="cham_sala" class="ui fluid dropdown">
+                          <option></option> 
+                        @foreach ($salas as $sala)
+                        <option value="{{$sala -> sala_id}}"> {{$sala -> sala_identificacao}}</option>
+                         @endforeach
+                    </select>
+                    </div>
+                     <div class="field">
+                         <label><b>Prazo para o concerto<b></label>
+                        <input type = "date" name="cham_data_prevista">   
+                    </select>
+                    </div>
+                </div>       
+                
                 <button class="ui button green" type="submit">Adicionar</button>
             </form>
               <a href="{{route('chamados.index')}}" style="float: right; margin-right:5em;"><i class="ui button blue"> Voltar</i></a> 
         </div>  
-
     </div>    
 
 </body>
