e94bc6a97293005c484ccd1832c54627ed636a07
kevinsousa <kevinsmoura@hotmail.com>
2019-03-30 13:37:29 -0300

    #4 Melhorando formulário

--- resources/views/user/_form.blade.php
+++ resources/views/user/_form.blade.php
@@ -3,19 +3,27 @@
     <input class="form-control" type="text" name="user_name" value="{{ isset($usuario->user_name) ? $usuario->user_name : ''}}" required="">
  </div>
 
- <div class="form-group">
- 	<label for="">CPF</label>
- 	<input class="form-control" type="text" name="user_cpf" value="{{ isset($usuario->user_cpf) ? $usuario->user_cpf : ''}}" required="" maxlength="11">
+ <div class="form-row">
+ 	<div class="form-group col-md-6">
+	 	<label for="">CPF</label>
+	 	<input class="form-control" type="text" name="user_cpf" value="{{ isset($usuario->user_cpf) ? $usuario->user_cpf : ''}}" required="" maxlength="11" id="CPF">
+ 	</div>
+
+	 <div class="form-group col-md-6">
+	 	<label for="">Número do SIAPE </label>
+	 	<input class="form-control" type="text" name="user_numero_siap" value="{{ isset($usuario->user_numero_siap) ? $usuario->user_numero_siap : ''}}" required="" maxlength="7">
+	 </div>
  </div>
 
- <div class="form-group">
- 	<label for="">Número do SIAPE </label>
- 	<input class="form-control" type="text" name="user_numero_siap" value="{{ isset($usuario->user_numero_siap) ? $usuario->user_numero_siap : ''}}" required="" maxlength="7">
- </div>
-
- <div class="form-group">
- 	<label for="">Email</label>
- 	<input class="form-control" type="email" name="user_email" value="{{ isset($usuario->user_email) ? $usuario->user_email : ''}}" required="">
+ <div class="form-row">
+	 <div class="form-group col-md-6">
+	 	<label for="">Email</label>
+	 	<input class="form-control" type="email" name="user_email" value="{{ isset($usuario->user_email) ? $usuario->user_email : ''}}" required="">
+	 </div>
+	 <div class="form-group col-md-6">
+	 	<label for="">Celular</label>
+	 	<input class="form-control" type="text" name="">
+	 </div>
  </div>
 
 <div class="form-group">
@@ -29,7 +37,15 @@
 	</select>
 </div>
 
-<div class="form-group">
-	<label> Senha </label>
-	<input class="form-control" type="password" name="user_password" required="">
+<div class="form-row">
+
+	<div class="form-group col-md-6">
+		<label> Senha </label>
+		<input class="form-control" type="password" name="user_password" required="" maxlength="16" minlength="8">
+	</div>
+
+	<div class="form-group col-md-6">
+		<label for=""> Repita a Senha</label>	
+		<input class="form-control" type="password" name="" maxlength="16" minlength="8">
+	</div>
 </div>
