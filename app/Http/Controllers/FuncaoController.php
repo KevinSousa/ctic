592478dad8857fe913b6bfaab629c39a748a82a8
kevinsousa <kevinsmoura@hotmail.com>
2019-03-30 09:44:24 -0300

    Organizando Código

--- app/Http/Controllers/FuncaoController.php
+++ app/Http/Controllers/FuncaoController.php
@@ -9,10 +9,14 @@ use App\Funcao;
 class FuncaoController extends Controller
 {
 	public function index() {
-     $funcao = Funcao::all();
+
+     	$funcao = Funcao::all();
         return view ('funcao', compact('funcao'));
+
     }
-    	public function store(Request $request) {
+    
+    public function store(Request $request) {
+
 		/*Validando os dados*/
 		$validar 			= 	$request->validate([
 			'funcao_name' 			=> 'required',
@@ -25,32 +29,40 @@ class FuncaoController extends Controller
 
 		$request->session()->flash('alert-success', 'Função cadastrada com Sucesso!');
 		return redirect('/funcao');
+
 	}
+
 	public function edit($id) {
+
 		/*Redireciona para o View editar com todos os dados do evento selecionado*/
 		$funcao = Funcao::where('funcao_id','=', $id)->first();
 		return view('editar-funcao', compact('funcao'));
+
 	}
 
 	public function update(Request $request, $id) {
+
 		/*Validando os dados*/
 		$validar 			= 	$request->validate([
 			'funcao_name' 			=> 'required',
 		],[ 'funcao_name.required' => 'Preencha o nome da Função']);
 
 		/* Se o Id for igual ao id da Função pega tudo*/
-		$funcao		 			= Funcao::where('funcao_id','=', $id)->first();
-		$funcao->funcao_name 	= $request->funcao_name;
+		$funcao	= Funcao::where('funcao_id','=', $id)->first();
+		$funcao->funcao_name = $request->funcao_name;
 		$funcao->save();
 
 		$request->session()->flash('alert-update', 'Evento Atualizado com sucesso!');
 		return redirect('/funcao');
+
 	}
 
 	public function destroy($id) {
+
 		/*Pega o item pelo id e destroi*/
 		$funcao = Funcao::where('funcao_id','=',$id);
 		$funcao->delete();
 		return redirect('/funcao');
+
 	}
 }
