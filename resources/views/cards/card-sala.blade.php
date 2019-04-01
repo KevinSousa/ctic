293d2c5c4918b2ee01265bfbd3a53fb47b855c69
Erickson <erickson.rinho@hotmail.com>
2019-04-01 01:22:40 -0300

    #4 - Modificando o Visual do Card Sala

--- resources/views/cards/card-sala.blade.php
+++ resources/views/cards/card-sala.blade.php
@@ -1,13 +1,15 @@
-<div class="field">
-    <label>Identificação da Sala</label>
-    <input type="text" name="sala_identificacao" value="{{ isset($sala->sala_identificacao) ? $sala->sala_identificacao : '' }}" placeholder="" required="">
+<div class="form-row">
+	<div class="col">	
+    	<label>Identificação da Sala</label>
+    	<input type="text" name="sala_identificacao" value="{{ isset($sala->sala_identificacao) ? $sala->sala_identificacao : '' }}" placeholder="" required="" class="form-control">
+	</div>
+    <div class="col">	
+	    <label>Andar/Bloco</label>
+	    <select name="sala_andar" class="form-control">
+	        <option value=""></option> 
+	        <option value="BLOCO A">BLOCO A</option>
+        	<option value="BLOCO B">BLOCO B</option>
+        	<option value="BLOCO C">BLOCO C</option>
+    	</select>
+    </div>   
 </div>
-<div class="field">
-    <label>Andar/Bloco</label>
-    <select name="sala_andar" class="ui fluid dropdown" required="">
-        <option></option>                    
-        <option value="BLOCO A">BLOCO A</option>
-        <option value="BLOCO B">BLOCO B</option>
-        <option value="BLOCO C">BLOCO C</option>
-    </select>
-</div> 
