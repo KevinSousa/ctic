1d8c64193b46910fd4772536f38f54a105eebe95
Erickson <erickson.rinho@hotmail.com>
2019-04-01 17:31:38 -0300

    #22 -Finalizando o CRUD

--- resources/views/tipos_problemas/card-tiposProblemas.blade.php
+++ resources/views/tipos_problemas/card-tiposProblemas.blade.php
@@ -1,7 +1,7 @@
 <div class="form-row" style="display:inline;">
 	<div class="col-4" align="center">
     	<label >Nome do Tipo de Problema</label>
-			<input type="text" name="problem_tipo" value="{{ isset($tipoProblema->problem_tipo) ? $tipoProblema->problem_tipo : '' }}" required="" class="form-control">
+			<input type="text" name="probl_tipo" value="{{ isset($tipoProblema->probl_tipo) ? $tipoProblema->probl_tipo : '' }}" required="" class="form-control">
     	<br>
 	</div>
 </div>
