ca47a014c9a59119a8e5a53e451940617d8da13b
Erickson <erickson.rinho@hotmail.com>
2019-04-01 01:20:12 -0300

    #4 - Alterando o template das Salas

--- resources/views/salas/adicionar.blade.php
+++ resources/views/salas/adicionar.blade.php
@@ -1,27 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.css">
+@extends('layouts.app')
+@section('content')
     <title>Salas</title>
-    <style type="text/css">
-    body {
-        margin-top: 25px;
-        margin-bottom: 30px;
-    }
-</style>
-</head>
-<body>
-    <div class="ui container">
-        <h1>Cadastro de Salas</h1>
+<div id="index">
+    <h2 id="titulo" align="left">Cadastro de Salas</h2>
+    <br>
         <div>
             <form action="{{ route('sala.salvar') }}" method="POST" class="ui form">
                 {{ csrf_field() }}
                 @component('cards/card-sala')
                 @endcomponent       
-                <button class="ui button green" type="submit">Adicionar</button>
+        <br>
+        <button class="btn btn-success" type="submit">Adicionar</button>
             </form>
         </div>    
     </div>    
-</body>
-</html>
+@endsection
+<style type="text/css">
+    div#index{
+        margin: 0px 25px 0px 25px;
+    }
+
+    h2#titulo {
+        color: #666;
+    }
+</style>
