abe987a034a9d4097845976abbadf0a331bdc1ac
Erickson <erickson.rinho@hotmail.com>
2019-04-01 17:31:38 -0300

    #22 -Finalizando o CRUD

--- resources/views/tipos_problemas/editar-tiposProblema.blade.php
+++ resources/views/tipos_problemas/editar-tiposProblema.blade.php
@@ -1,34 +1,17 @@
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title> CTIC </title>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.css">
-</head>
-<style type="text/css">
-	@import url('https://fonts.googleapis.com/css?family=Acme');
-	*{
-		font-family: 'Acme', sans-serif;
-	}
-	body {
-		margin-top: 25px;
-		margin-bottom: 30px;
-	}
-</style>
-<body>
-	<div class="ui container">
-		<h1> Alterar Dados </h1>
-		<h3> Alteração de dados de tipos de problemas </h3>
-		<div>
-			<form method="post" action="{{route('funcao.update', $funcao->funcao_id)}}" class="ui form">
+@extends('layouts.app')
+@section('content')
+    <title>Tipos Problemas</title>
+<div id="index">
+    <h2 id="titulo" align="left">Editar Tipos de Problemas</h2>
+    <br>
+    <div align="center">
+			<form method="post" action="{{route('tiposProblemas.update', $tipoProblema->probl_id)}}" class="ui form">
 				{{ csrf_field() }}
 				<input type="hidden" name="_method" value="put">
 				@component('tipos_problemas/card-tiposProblemas', ['tipoProblema' => $tipoProblema])
 				@endcomponent
-				<input class="ui primary button" type="submit" name="" value="Atualizar">
-				<input class="ui button" type="reset" name="" value="Limpar">
+				<button class="btn btn-success" type="submit"> Atualizar </button>
 			</form>
 		</div>
-	</div>
-</body>
-</html>
+</div>
+@endsection
