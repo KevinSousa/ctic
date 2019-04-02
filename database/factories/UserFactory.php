3a7db3295824ea213504a9b1da32f5de08df7f2f
Erickson <erickson.rinho@hotmail.com>
2019-04-02 13:27:32 -0300

    #30 - Randorizando as funções na Factory user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -12,6 +12,6 @@ $factory->define(App\User::class, function (Faker $faker) {
         'user_email' => $faker->unique()->safeEmail,
         'user_password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
-        'user_funcao'   => '1',
+        'user_funcao'   => Funcao::get()->random()->funcao_id,
     ];
 });
