2b19d4bd9307b6d4a87923a729ee771ce3f6906f
Erickson <erickson.rinho@hotmail.com>
2019-04-02 01:12:39 -0300

    #30 - Adicionando o Seeder e o Factory de Users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,26 +1,17 @@
 <?php
 
-use App\User;
-use Illuminate\Support\Str;
 use Faker\Generator as Faker;
+use App\Funcao;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
-
-$factory->define(User::class, function (Faker $faker) {
+$factory->define(App\User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'user_name' => $faker->name,
+        'user_cpf' => '12345678912',
+        'user_telefone' => $faker->phoneNumber,
+        'user_numero_siap' => $faker->phoneNumber,
+        'user_email' => $faker->unique()->safeEmail,
+        'user_password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'remember_token' => str_random(10),
+        'user_funcao'   => '1',
     ];
 });
