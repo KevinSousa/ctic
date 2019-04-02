922184dd8748e21822e53031ecbc1ee051d9eedf
kevinsousa <kevinsmoura@hotmail.com>
2019-04-02 08:29:12 -0300

    Melhorando form de Usuários

--- resources/views/user/_form.blade.php
+++ resources/views/user/_form.blade.php
@@ -1,27 +1,27 @@
 <div class="form-group">
-    <label for="">Nome Completo</label>
+    <label for="">Nome Completo *</label>
     <input class="form-control" type="text" name="user_name" value="{{ isset($usuario->user_name) ? $usuario->user_name : ''}}" required="">
 </div>
 
 <div class="form-row">
 	<div class="form-group col-md-6">
-		<label for="">CPF</label>
+		<label for="">CPF *</label>
 		<input class="form-control" type="text" name="user_cpf" value="{{ isset($usuario->user_cpf) ? $usuario->user_cpf : ''}}" required="" maxlength="14" id="cpf">
 	</div>
 
 	<div class="form-group col-md-6">
-		<label for="">Número do SIAPE </label>
+		<label for="">Número do SIAPE *</label>
 		<input class="form-control" type="text" name="user_numero_siap" value="{{ isset($usuario->user_numero_siap) ? $usuario->user_numero_siap : ''}}" required="" maxlength="7">
 	</div>
 </div>
 
 <div class="form-row">
 	 <div class="form-group col-md-6">
-	 	<label for="">Email</label>
+	 	<label for="">Email *</label>
 	 	<input class="form-control" type="email" name="user_email" value="{{ isset($usuario->user_email) ? $usuario->user_email : ''}}" required="">
 	 </div>
 	 <div class="form-group col-md-6">
-	 	<label for="">Celular</label>
+	 	<label for="">Celular *</label>
 	 	<input class="form-control" type="text" name="" id="telefone">
 	 </div>
 </div>
@@ -40,12 +40,12 @@
 <div class="form-row">
 
 	<div class="form-group col-md-6">
-		<label> Senha </label>
+		<label> Senha *</label>
 		<input class="form-control" type="password" name="user_password" required="" maxlength="16" minlength="8">
 	</div>
 
 	<div class="form-group col-md-6">
-		<label for=""> Repita a Senha</label>	
+		<label for=""> Repita a Senha *</label>	
 		<input class="form-control" type="password" name="" maxlength="16" minlength="8">
 	</div>
 </div>
