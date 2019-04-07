3e529a8a97142dc79da85cb75032c6cca9c09d17
KevinSousa <kevinsmoura@hotmail.com>
2019-04-07 10:30:09 -0300

    #5 Login

--- app/User.php
+++ app/User.php
@@ -19,12 +19,18 @@ class User extends Authenticatable
     protected $guarded  = ['user_id'];
     
     /*nome dos atributos que poderão ser alterados*/
-    protected $fillable = ['user_name','user_funcao','user_email', 'user_password', 'user_cpf', 'user_siap_matricula', 'user_telefone'];
+    protected $fillable = ['user_name','user_funcao','user_email', 'password', 'user_cpf', 'user_siap_matricula', 'user_telefone'];
 
-    protected $hidden = ['user_password', 'remember_token'];
+    protected $hidden = ['password', 'remember_token'];
+
+    // public function setPasswordAttribute($password) {
+    //     $this->attributes['password'] = bcrypt($password);
+    // }
 
     // protected $casts = ['email_verified_at' => 'datetime'];
-
+    public function setPasswordAttribute($password){
+        $this->attributes['password'] = bcrypt($password);
+    }
     /*Função que representa o relacionamento de um para muitos*/
      public function user_funcao(){
          return $this->hasMany(Funcao::class);
