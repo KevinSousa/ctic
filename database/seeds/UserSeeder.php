a4de23faa672f8cb9f483264f86e19293f8fae1b
Erickson <erickson.rinho@hotmail.com>
2019-04-07 01:43:27 -0300

    #30 - Adicionando um Usuario Admin no UserSeeder, mas está comentado

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -13,7 +13,16 @@ class UsersSeeder extends Seeder
     {
         //
         factory(App\User::class, 10)->create()->each(function ($user) {
-            // $user->posts()->save(factory(App\Post::class)->make());
         });
+        // DB::table('users')->insert([
+        // 'user_name' => 'admin',
+        // 'user_cpf' => '111111111111',
+        // 'user_telefone' => '999999999',
+        // 'user_siap_matricula' => 'aaa123',
+        // 'user_email' => 'admin@ctic.ifpe',
+        // 'user_password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        // 'remember_token' => str_random(10),
+        // 'user_funcao'   => '1',
+        // ]);
     }
 }
