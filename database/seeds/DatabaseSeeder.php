72d3b9de4a266a34bf2d31d6d884cf56f65baf5d
Erickson <erickson.rinho@hotmail.com>
2019-04-08 02:19:02 -0300

    #30 - Ordenando da maneira correta DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
          $this->call(SalaSeeder::class);
          $this->call(Tipo_equipamentoSeeder::class);
          $this->call(EquipamentoSeeder::class);
-         $this->call(ChamadoSeeder::class);
          $this->call(Tipo_problemaSeeder::class);
+         $this->call(ChamadoSeeder::class);
     }
 }
