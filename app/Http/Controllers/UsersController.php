825d679bbff9510474c481935bab7125652cec0a
KevinSousa <kevinsmoura@hotmail.com>
2019-04-07 23:30:37 -0300

    #5 Finalização da Autenticação

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,10 +10,7 @@ use App\Funcao;
 
 class UsersController extends Controller
 {
-    
-    public function __construct(){
-        $this -> middleware('auth');
-    }
+
     public function index(){
 
         $users = DB::table('users')
