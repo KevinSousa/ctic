e9e814b22966d559eb4903dbcb7d3ccc549c0a2b
Luigimartins <luigi.martins.355@gmail.com>
2019-04-10 01:41:32 -0300

    #40-mudança dos dados da factory de tipos_equips

--- database/factories/Tipo_equipamentoFactory.php
+++ database/factories/Tipo_equipamentoFactory.php
@@ -5,6 +5,6 @@ use app\Tipo_equipamento;
 
 $factory->define(App\Tipo_equipamento::class, function (Faker $faker) {
     return [
-        'tipo_nome' => $faker->randomElement(['Hardware','Software', 'Rede']),
+        'tipo_nome' => $faker->randomElement(['Computador','Mouse', 'Teclado', 'Access Point','Projetor', 'No Break']),
     ];
 });
