0fc2763111222b1c54d5c64a3d198f063eeb623c
Erickson <erickson.rinho@hotmail.com>
2019-04-15 17:37:12 -0300

    #30 - Adicionando dados reais em Tipo_problemaSeeder

--- database/seeds/Tipo_problemaSeeder.php
+++ database/seeds/Tipo_problemaSeeder.php
@@ -11,8 +11,17 @@ class Tipo_problemaSeeder extends Seeder
      */
     public function run()
     {
-    	 factory(App\Tipo_problema::class, 1)->create()->each(function ($user) {
+    	 factory(App\Tipo_problema::class, 0)->create()->each(function ($user) {
 		
 		});
+         DB::table('tipo_problemas')->insert([
+        'probl_tipo' => 'Hardware',
+        ]);      
+         DB::table('tipo_problemas')->insert([
+        'probl_tipo' => 'Software',
+        ]);      
+         DB::table('tipo_problemas')->insert([
+        'probl_tipo' => 'Rede',
+        ]);
     }
 }
