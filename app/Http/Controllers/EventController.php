74467cc2275c4251fde0e5dcd89b625adb064d30
kevinsousa <kevinsmoura@hotmail.com>
2019-04-19 18:56:03 -0300

    #26 Criando método de agendamento

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -29,4 +29,31 @@ class EventController extends Controller
         return view('calendar.index', compact('calendar_details'));
 
     }
+
+    public function addEvent(){
+        return view('calendar.addEvent');   
+    }
+
+    public function saveEvent(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'event_name' => 'required',
+            'start_date' => 'required',
+            'end_date' => 'required'
+        ]);
+
+        if($validator->fails()){
+            \Session::flash('warning', 'Erro ao efetuar agendamento');
+            return Redirect::to('/calendar/addEvent')->withInput()->withErrors($validator);
+        }
+
+        $event = new Event;
+        $event->event_name = $request['event_name'];
+        $event->start_date = $request['start_date'];
+        $event->end_date = $request['end_date'];
+        $event -> save();
+
+        \Session::flash('success', 'Agendamento efetuado com sucesso.');
+        return Redirect::to('/calendar/addEvent');
+    }
 }
