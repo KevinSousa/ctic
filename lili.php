f90be245fc6b9180b9974f754594536f5eab0ecc
jackson29 <jacksonssantos@bol.com.br>
2019-04-24 09:27:43 -0300

    #39 -Coleção de dados do usuário

---
+++ lili.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Entrevista realizada por meio do método "GRUPO DE FOCO".</title>
+</head>
+<body>
+	<h1>Entrevista realizada por meio do método "GRUPO DE FOCO".</h1>
+
+	<h2><i>Quais as funcionalidades que você gostaria e as acharia conveniente para um projeto deste fim?</i></h2>
+	<p>Resposta:<br>
+	 – Agendamento do laboratório;<br>
+	 – Registro de ocorrências;<br>
+	 – Visualização dos chamados abertos;<br>
+	 – Notificação por SMS. </p><br>
+
+	<h2><i>O que você conhece de projetos semelhantes, ou seja, o que eles têm, e como poderiam ser melhorados?</i></h2>
+	<p>Resposta:<br>
+		– Fluxograma;<br>
+	– Status do chamado;<br>
+    – Atratividade no front-end;<br>
+    – Cadastro efetuado somente com e-mail válido;<br>
+    – Estrutura da matrícula no exercício de restrição;<br>
+    – Opção de acompanhamento pelo e-mail. </p><br>
+
+	<h2><i>O que você acharia estupendo nesse projeto? </i> </h2>
+    <p>Resposta:<br>
+    – Bate-papo.</p>
+
+    <H2><i>O que seria inaceitável?</i></H2>
+    <p>Resposta:<br>
+    – Criação de contas sem restrições.</p>
+</body>
+</html>
