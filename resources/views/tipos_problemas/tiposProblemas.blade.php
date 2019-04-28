5c5bbf9781a4a2f809ac572d84195724526bf56d
Luigi Martins <luigi.martins.355@gmail.com>
2019-04-28 05:32:29 -0300

    #53-mudando view para ser usada com a requisição async

--- resources/views/tipos_problemas/tiposProblemas.blade.php
+++ resources/views/tipos_problemas/tiposProblemas.blade.php
@@ -1,7 +1,5 @@
-@extends('layouts.app')
-
-@section('content')
-	<div id="index">
+	<title>Tipos de Problemas</title>
+    <div id="index">
 		<div align="left">	
 			<h1 id="titulo"> Tipos de Problemas </h1>
 			<br>
@@ -35,32 +33,7 @@
 		</table>
 		<br>
 	</div>
-@endsection
-@section('js')
-     <!-- Jquery JS-->
-        <script src="/vendor/jquery-3.2.1.min.js"></script>
-        <!-- Bootstrap JS-->
-        <script src="/vendor/bootstrap-4.1/popper.min.js"></script>
-        <script src="/vendor/bootstrap-4.1/bootstrap.min.js"></script>
-        <!-- Vendor JS       -->
-        <script src="/vendor/slick/slick.min.js">
-        </script>
-        <script src="/vendor/wow/wow.min.js"></script>
-        <script src="/vendor/animsition/animsition.min.js"></script>
-        <script src="/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
-        </script>
-        <script src="/vendor/counter-up/jquery.waypoints.min.js"></script>
-        <script src="/vendor/counter-up/jquery.counterup.min.js">
-        </script>
-        <script src="/vendor/circle-progress/circle-progress.min.js"></script>
-        <script src="/vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
-        <script src="/vendor/chartjs/Chart.bundle.min.js"></script>
-        <script src="/vendor/select2/select2.min.js">
-        </script>
-        <!-- Main JS-->
-        <script src="/js/main.js"></script>
 
-@endsection
 <style type="text/css">
 	div#index{
 		margin: 0px 25px 0px 25px;
