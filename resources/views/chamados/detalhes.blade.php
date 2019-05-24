ea8cadf2e1ba75069c338b60272f7793878bf9b7
jhonnyfreitas1 <jhonnyfarias87@gmail.com>
2019-05-23 23:45:36 -0300

    adicionando layout a pagina de detalhes e esperando o ajax

--- resources/views/chamados/detalhes.blade.php
+++ resources/views/chamados/detalhes.blade.php
@@ -1,31 +1,11 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title> CTIC - Chamados </title>
+@extends( (!$ajax) ? 'layouts.app' : 'layouts.ajax')
+@section('content')
 		  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.css">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 		 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	</head>
-		<style type="text/css">
-			@import url('https://fonts.googleapis.com/css?family=Acme');
-			*{
-				font-family: 'Acme', sans-serif;
-			}
-			body {
-				margin-top: 25px;
-				margin-bottom: 30px;
-			}
-			tr{
-				position: center;
-			}
-			
-			td{
-				font-size: 110%;
-				width: 10em;
 
-			}
-			
+		<style type="text/css">
 	        .destaque{
    			border-style: ridge;
    			font-size: 115%;
@@ -35,7 +15,7 @@
                         
                         
 			<!---- DESTALHES DO CHAMADO DIFERENÇA DA LISTA É QUE DIZ O DIA DO CHAMADO E DATA PREVISTA-->
-	<body>
+
 			<div class="ui container">
 				<h1>Destalhes do chamado</h1>
 				<div>
@@ -97,8 +77,7 @@
 
 			</table>
 				</div>
-				<a href="{{route('chamados.index')}}">Voltar</a>
-				<br>
+				
 			</div>
 			<script type="text/javascript">
 				$(function(){
@@ -111,6 +90,57 @@
 					    }
 					    );
 					});
-			</script>
-</body>
-</html>
+					</script>
+			@endsection
+
+@section('js')
+     <!-- Jquery JS-->
+        <script src="/vendor/jquery-3.2.1.min.js"></script>
+        <!-- Bootstrap JS-->
+        <script src="/vendor/bootstrap-4.1/popper.min.js"></script>
+        <script src="/vendor/bootstrap-4.1/bootstrap.min.js"></script>
+        <!-- Vendor JS       -->
+        <script src="/vendor/slick/slick.min.js">
+        </script>
+        <script src="/vendor/wow/wow.min.js"></script>
+        <script src="/vendor/animsition/animsition.min.js"></script>
+        <script src="/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
+        </script>
+        <script src="/vendor/counter-up/jquery.waypoints.min.js"></script>
+        <script src="/vendor/counter-up/jquery.counterup.min.js">
+        </script>
+        <script src="/vendor/circle-progress/circle-progress.min.js"></script>
+        <script src="/vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
+        <script src="/vendor/chartjs/Chart.bundle.min.js"></script>
+        <script src="/vendor/select2/select2.min.js">
+        </script>
+        <!-- Main JS-->
+        <script src="/js/main.js"></script>
+        <script src="/js/select-sublistas.js"></script>
+
+        <script>
+            $(document).ready( function(){
+                $('#adc-chamado').parent('li').addClass("active");
+            });
+        </script>
+
+
+@endsection
+@section('ajax-js')
+    <script src="/js/select-sublistas.js"></script>
+
+@endsection
+<style type="text/css">
+    div#index{
+        margin: 0px 25px 0px 25px;
+    }
+
+    h2#titulo {
+        color: #666;
+    }
+
+    .form-row, .form-group{
+        text-align: left;
+    }
+
+</style>
