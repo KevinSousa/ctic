411473e9f7aee17cb85d2042192734f308079aaf
Erickson <erickson.rinho@hotmail.com>
2019-05-25 09:25:13 -0300

    #71 - Adicionando o novo padrão em adicionar Função

--- resources/views/funcao/adc-editar-funcao.blade.php
+++ resources/views/funcao/adc-editar-funcao.blade.php
@@ -2,35 +2,59 @@
 
 @section('content')
 <title>Funções</title>
-<div id="index" style="width: 45em;height: 30em">
-
-	@isset($funcao)
-   		<h2 id="titulo" align="left"> Editar Função </h2>
-    @else
-    	<h2 id="titulo" align="left"> Cadastro de Função </h2>
-    @endisset
-    <br>	
-	@isset($funcao)
-		<form method="post" action="{{route('funcao.update', $funcao->funcao_id)}}" class="ui form">
-	@else
-		<form method="post" action="{{route('funcao.store')}}" class="ui form">
-	@endisset
-		{{ csrf_field() }}
-		<input type="hidden" name="_method" value="put">
-		<div class="form-group">
-            <label>Digite o nome da Função:</label>
+<style> 
+ img{
+    width: 60%;
+    -webkit-transition: all 0.6s;
+    -o-transition: all 0.6s;
+    -moz-transition: all 0.6s;
+    transition: all 0.6s;
+}
+ img:hover {
+    -webkit-transform: scale(1.1);
+    -moz-transform: scale(1.1);
+    -ms-transform: scale(1.1);
+    -o-transform: scale(1.1);
+    transform: scale(1.1);
+}
+</style>
+<div class="container-fluid">
+    <div class="row h-100 p-3" style="margin-top: -11%" >
+        <div class="col h-100 p-3" style="margin: 0 auto">
+            <img src="{{asset('img-01.png')}}" alt="" style="margin-top: 35%; margin-left: 12%">
+        </div>
+        <div class="col">
+            <div style="margin-top: 30%;">
+        	@isset($funcao)
+           		<h2 id="titulo" align="left"> Editar Função </h2>
+            @else
+            	<h2 id="titulo" align="left"> Cadastro de Função </h2>
+            @endisset
             <br>	
-			<input type="text" name="funcao_name"  value="{{ isset($funcao->funcao_name) ? $funcao->funcao_name : '' }}" placeholder="Ex: Administrador" required="" class="form-control">
-		</div>
-		@isset($funcao)
-			<button class="btn btn-success" type="submit">Editar Função</button>
-       		<a href="{{route('chamados.index')}}">
-        		<button class="btn btn-primary">Voltar</button>
-        	</a>
-		@else
-		    <button class="btn btn-success" type="submit">Adicionar Função</button>
-		@endisset
-		</form>
+        	@isset($funcao)
+        		<form method="post" action="{{route('funcao.update', $funcao->funcao_id)}}" class="ui form">
+        	@else
+        		<form method="post" action="{{route('funcao.store')}}" class="ui form">
+        	@endisset
+        		{{ csrf_field() }}
+        		<input type="hidden" name="_method" value="put">
+        		<div class="form-group">
+                    <label>Digite o nome da Função:</label>
+                    <br>	
+        			<input type="text" name="funcao_name"  value="{{ isset($funcao->funcao_name) ? $funcao->funcao_name : '' }}" placeholder="Ex: Administrador" required="" class="form-control">
+        		</div>
+        		@isset($funcao)
+        			<button class="btn btn-success" type="submit">Editar Função</button>
+               		<a href="{{route('chamados.index')}}">
+                		<button class="btn btn-primary">Voltar</button>
+                	</a>
+        		@else
+        		    <button class="btn btn-success" type="submit">Adicionar Função</button>
+        		@endisset
+        		</form>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
 @section('js')
