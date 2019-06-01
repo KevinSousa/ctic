78138ebfd6979f1d9554a674ac8b3188b7189451
kevinsousa <kevinsmoura@hotmail.com>
2019-06-01 11:17:17 -0300

    #81 Criando gráfico para mostrar chamados por tipos de problemas

---
+++ app/Http/Controllers/ChartController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Khill\Lavacharts\Lavacharts;
+use Illuminate\Http\Request;
+use App\TiposProblemasController;
+use Illuminate\Support\Facades\DB;
+
+class ChartController extends Controller
+{
+    public function index(){
+
+		$lava = new Lavacharts; // See note below for Laravel
+
+		$hardware = DB::table('chamados')
+            ->join('sublista_tipo_problemas', 'chamados.cham_sublista_problema', '=', 'sublista_tipo_problemas.sub_id')
+            ->join('tipo_problemas', 'sublista_tipo_problemas.sub_probl', '=', 'tipo_problemas.probl_id')
+            ->where('tipo_problemas.probl_id', '=', '1')
+            ->count();
+
+		$software = DB::table('chamados')
+		            ->join('sublista_tipo_problemas', 'chamados.cham_sublista_problema', '=', 'sublista_tipo_problemas.sub_id')
+		            ->join('tipo_problemas', 'sublista_tipo_problemas.sub_probl', '=', 'tipo_problemas.probl_id')
+		            ->where('tipo_problemas.probl_id', '=', '2')
+		            ->count();
+				
+		$redes = DB::table('chamados')
+		            ->join('sublista_tipo_problemas', 'chamados.cham_sublista_problema', '=', 'sublista_tipo_problemas.sub_id')
+		            ->join('tipo_problemas', 'sublista_tipo_problemas.sub_probl', '=', 'tipo_problemas.probl_id')
+		            ->where('tipo_problemas.probl_id', '=', '3')
+		            ->count();
+
+		$reasons = $lava->DataTable();
+
+		$reasons->addStringColumn('Reasons')
+		        ->addNumberColumn('Percent')
+		        ->addRow(['Hardware', $hardware])
+		        ->addRow(['Software', $software])
+		        ->addRow(['Rede', $redes]);
+
+		$lava->DonutChart('IMDB', $reasons, [
+		    'title' => 'Percentual de Chamados por tipo',
+		    'width' => 1000,
+        	'height' => 1000,
+        	'legend' => [
+        		'position' => 'left'
+        	]
+		]);
+
+	    return View('chart.index', compact('lava'));
+
+
+    }
+
+}
---
+++ resources/views/chart/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<div id="poll-div">
+
+	<?=($lava->render('DonutChart', 'IMDB', 'poll-div'))?>
+	
+</div>
+
+
+</body>
+</html>
