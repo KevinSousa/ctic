d1d3270571ddd9009ca607c155166c36b301d08b
Erickson <erickson.rinho@hotmail.com>
2019-06-02 11:50:46 -0300

    #51 - Editando o Datatables na index de chamados

--- resources/views/chamados/index.blade.php
+++ resources/views/chamados/index.blade.php
@@ -1,40 +1,28 @@
 @extends( (!$ajax) ? 'layouts.app' : 'layouts.ajax')
 
 @section('content') 
-	<title> Chamados </title>
+    <title> Chamados </title>
     <div id="index">
         <div align="left">  
             <h1 id="titulo">Chamados</h1>
             <br>
         </div>
         <br>
-        <table class="table table-striped" id="example">
-            <thead class="thead-light">
-                <tr align="center">
-		            <th scope="col">AUTOR</th>
-		            <th scope="col">GRAU DE URGÊNCIA</th>
-                    <th scope="col">TIPO DO PROBLEMA</th>
-		            <th scope="col">STATUS</th>
-		            <th scope="col">AÇÕES</th>
-		        </tr>
-		    </thead>
-		    <tbody>
-		    	@foreach ($chamados as $chamado)
-					<tr align="center">
-						<td> {{$chamado -> user_name}} </td>
-						<td> {{$chamado -> cham_grau_urgencia}} </td>
-                        <td> {{$chamado -> sub_nome}}</td>
-						<td> {{$chamado -> cham_status}}</td>
-						<td> 	
-						<a href="{{route('chamados.detalhes',$chamado->cham_id)}}"><i class="ui primary button ">Detalhes</i></a>
-					</td>
-					</tr>
-				@endforeach
-		    </tbody>
-		</table>
-		<br>
-		{{$chamados->links()}}
-	</div>
+        <div class="box-body"></div>
+            <table class="table table-condensed table-hover dataTable table-orderdesc" id="example">
+                <thead >
+                    <tr align="center">
+                        <th>AUTOR</th>
+                        <th>GRAU DE URGÊNCIA</th>
+                        <th>TIPO DO PROBLEMA</th>
+                        <th>STATUS</th>
+                        <th>AÇÕES</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+        <br>
+    </div>
 @endsection
 @section('js')
      <!-- Jquery JS-->
@@ -59,32 +47,74 @@
         </script>
         <!-- Main JS-->
         <script src="/js/main.js"></script>
+        <!-- DataTables JS-->
+        <script src="https://datatables.yajrabox.com/js/jquery.min.js"></script>
+        <script src="https://datatables.yajrabox.com/js/bootstrap.min.js"></script>
+        <script src="https://datatables.yajrabox.com/js/jquery.dataTables.min.js"></script>
+        <script src="https://datatables.yajrabox.com/js/datatables.bootstrap.js"></script>
 
-        <script type="text/javascript">
+         <script type="text/javascript">
             $(document).ready( function (){
-                $('#example').DataTable();
                 $('#vis-menu').click();
                 $('#visu-chamados').parent('li').addClass("active");
+
+                // Datatable
+                $('#example').DataTable({
+                    bProcessing: true,
+                    deferRender: true,
+                    serverSide: true,
+                    oLanguage:{
+                        sProcessing: "Processando...",
+                        sLengthMenu: "Mostar _MENU_ registros pro página",
+                        sZeroRecords: "Nada encontrado com esse critérios",
+                        sEmptyTable: "Não há dados para serem mostrados",
+                        sLoadingRecords: "Carregando...",
+                        sInfo: "Mostrando _START_ a _END_ de _TOTAL_ registros",
+                        sInfoEmpty: "Mostrando 0 até 0 de 0 registros",
+                        sInfoFiltered: "(filtro aplicado em _MAX_ registros)",
+                        sInfoPostFix: "",
+                        sInfoThousands: ".",
+                        sSearch: "Pesquisar:",
+                        sUrl: "",
+                            oPaginate:{
+                                sFirst: "Primeira",
+                                sPrevious: "Anterior",
+                                sNext: "Próxima",
+                                sLast: "Última",
+                            },
+                    },
+                    // bPaginate: false, //Next and Previous embaixo da tabela
+                    // bLengthChange: false,  //Show and entries em cima da tabela
+                    // bFilter: true, //Search em cima da tabela
+                    // bInfo: false,  //Showing em baixo da tabela
+                    ajax: "{{ route('chamados.getchamados')}}",
+                    columns: [
+                            {data:'user_name'},
+                            {data:'cham_grau_urgencia'},
+                            {data:'sub_nome'},
+                            {data:'cham_status'},
+                            {data:'cham_user'},
+                        ],
+                    pageLength: 10,
+                    sScrollx: '100%',
+                    sScrollxInner: '100%',
+                    aaSorting: [[0,'asc']],
+                }); 
             });
         </script>
-
         
 
 @endsection
 @section('ajax-js')
     
-    <script type="text/javascript">
-        $(document).ready( function (){
-            $('#example').DataTable();
-        });
-    </script>
+
 @endsection
 <style type="text/css">
-	div#index{
-		margin: 0px 25px 0px 25px;
-	}
+    div#index{
+        margin: 0px 25px 0px 25px;
+    }
 
-	#titulo {
-		color: #666;
-	}
+    #titulo {
+        color: #666;
+    }
 </style>
