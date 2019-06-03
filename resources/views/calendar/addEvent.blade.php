0df6c55ee4e881541a74da1b6d5acc93626de1fe
marcoapoli <marco-gordo@hotmail.com>
2019-06-03 16:26:00 -0300

    #71 - Padronizando agendar calendario

--- resources/views/calendar/addEvent.blade.php
+++ resources/views/calendar/addEvent.blade.php
@@ -1,42 +1,51 @@
-@extends( (!$ajax) ? 'layouts.app' : 'layouts.ajax')
+    @extends( (!$ajax) ? 'layouts.app' : 'layouts.ajax')
 @section('content')
-<div id="index">
-    <h1 align="left"> Agendar Laboratório </h1>
-    <br>
-    <div>
-        <form action="{{route('calendar.saveEvent')}}" method="post">
-            {{ csrf_field() }}
-            <div class="form-group">
-                @if (Session::has('success'))
-                    <div class="alert alert-success">{{ Session::get('success')}}</div>
-                @elseif (Session::has('warnning'))
-                    <div class="alert alert-danger">{{ Session::get('warnning')}}</div>
-                @endif
-                <label> Nome do Evento </label>
-                <input type="text" class="form-control"  name="event_name" required="">
+<title>Agendar Laboratório</title>
+<div class="container-fluid">
+    <div class="row h-100 p-3" style="margin-top: -11%" >
+        <div class="col">
+            <div style="margin-top: 30%;">
+                <h2 id="titulo" align="left">Agendar Laboratório</h2>
+                <br>
+                 <form action="{{route('calendar.saveEvent')}}" method="post">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        @if (Session::has('success'))
+                            <div class="alert alert-success">{{ Session::get('success')}}</div>
+                        @elseif (Session::has('warnning'))
+                            <div class="alert alert-danger">{{ Session::get('warnning')}}</div>
+                        @endif
+                        <label> Nome do Evento </label>
+                        <input type="text" class="form-control"  name="event_name" required="">
+                    </div>
+                    <div class="form-group">
+                        <label> Data de Início </label>
+                        <input type="datetime-local"   class="form-control" name="start_date" required="">
+                    </div>
+                    <div class="form-group">
+                        <label> Data de Término </label>
+                        <input type="datetime-local"  class="form-control" name="end_date" required="">
+                    </div>
+                    <button class="btn btn-success" type="submit"> Cadastrar </button>
+                    <a href="{{route('calendar')}}"><button class="btn btn-primary">Voltar</button></a>
+                </form>
             </div>
-            <div class="form-group">
-                <label> Data de Início </label>
-                <input type="datetime-local"   class="form-control" name="start_date" required="">
-            </div>
-            <div class="form-group">
-                <label> Data de Término </label>
-                <input type="datetime-local"  class="form-control" name="end_date" required="">
-            </div>
-            <button class="btn btn-success" type="submit"> Cadastrar </button>
-            <a href="{{route('calendar')}}"><button class="btn btn-primary">Voltar</button></a>
-        </form>
-    </div>
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-</div>
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif  
+        </div>
+        <div class="col h-100 p-3" style="margin: 0 auto; margin-top: -6em">
+            <img src="{{asset('img-04.png')}}" alt="" class="icons-adc">
+        </div>
+    </div>  
+</div> 
+
 @endsection
 
 @section('js')
@@ -74,16 +83,13 @@
 @endsection
 
 <style type="text/css">
-    div#index {
+     div#index{
         margin: 0px 25px 0px 25px;
     }
 
-    div#calendar {
-        margin: 0px 80px 10px 80px;
-        padding: 15px;
-        border-radius: 5px;
+    h2#titulo {
+        color: #666;
     }
-
     .form-group{
         text-align: left;
     }
