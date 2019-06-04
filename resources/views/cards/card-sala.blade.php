8221e9920772698aab55674541f00136ffe354dc
Erickson <erickson.rinho@hotmail.com>
2019-06-04 10:29:34 -0300

    #71 - Correção do card de salas

--- resources/views/cards/card-sala.blade.php
+++ resources/views/cards/card-sala.blade.php
@@ -1,3 +1,4 @@
+@php($bloco = ['BLOCO A','BLOCO B','BLOCO C'])
 <div class="form-group">
     	<label>Numero da Sala:</label>
         <input type="text" name="sala_identificacao" value="{{ isset($sala->sala_identificacao) ? $sala->sala_identificacao : '' }}" placeholder="Ex: 5" required="" class="form-control" id="field" maxlength="2"  max="10">
@@ -6,8 +7,13 @@
             <option disabled>Escolha o bloco onde a sala está localizada</option> 
             <option disabled>---</option> 
 	        <option value="" hidden></option> 
-	        <option value="BLOCO A">BLOCO A</option>
-        	<option value="BLOCO B">BLOCO B</option>
-        	<option value="BLOCO C">BLOCO C</option>
+	        @foreach ($bloco as $blocos)
+                <option value="{{$blocos}}"
+                    @if (isset($sala) && $sala->sala_andar == $blocos)
+                        Selected
+                    @endif>
+                        {{$blocos}}
+                    </option>
+            @endforeach
     	</select>
 </div>
