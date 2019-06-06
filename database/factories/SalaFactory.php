022aa02191ae4d042ef810cb846f689bd7064ece
Erickson <erickson.rinho@hotmail.com>
2019-06-06 05:20:24 -0300

    Adicionanado descricao na migration events e comentando algumas linhas na Factory de Salas

--- database/factories/SalaFactory.php
+++ database/factories/SalaFactory.php
@@ -5,9 +5,10 @@ use App\Sala;
 
 
 $factory->define(Sala::class, function (Faker $faker) {
-	$random = $faker->randomLetter.$faker->numberBetween(1, 5); 
+	// $letras = ['A','B','C'];
+	// $random = $faker->randomElement($letras).$faker->numberBetween(1, 5); 
+	// $random = $faker->numberBetween(1, 5); 
     return [
-        'sala_identificacao' => $random,
-        'sala_andar' => $faker->randomElement(['BLOCO A','BLOCO B', 'BLOCO C'])
+	
     ];
 });
