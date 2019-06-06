5110b45b3637d3b6e1d13c1c43ee78d2bc0eef49
Erickson <erickson.rinho@hotmail.com>
2019-06-06 08:40:57 -0300

    Adicionando o '*' nos campos obrigatorios tipos problemas

--- resources/views/tipos_problemas/card-tiposProblemas.blade.php
+++ resources/views/tipos_problemas/card-tiposProblemas.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group" style="display:inline;">
 
-    	<label >Nome do Tipo de Problema</label>
+    	<label >Nome do Tipo de Problema*</label>
 			<input type="text" name="probl_tipo" value="{{ isset($tipoProblema->probl_tipo) ? $tipoProblema->probl_tipo : '' }}" required="" placeholder="Ex: Hd" class="form-control">
     	<br>
 
