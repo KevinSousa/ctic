839ab87c7d23fe283bfac0cb56c64482f5ede930
Erickson <erickson.rinho@hotmail.com>
2019-06-06 08:38:50 -0300

    Adicionando o '*' nos campos obrigatorios do card-sala

--- resources/views/cards/card-sala.blade.php
+++ resources/views/cards/card-sala.blade.php
@@ -1,8 +1,8 @@
 @php($bloco = ['BLOCO A','BLOCO B','BLOCO C'])
 <div class="form-group">
-    	<label>Numero da Sala:</label>
+    	<label>Numero da Sala*</label>
         <input type="text" name="sala_identificacao" value="{{ isset($sala->sala_identificacao) ? $sala->sala_identificacao : '' }}" placeholder="Ex: 5" required="" class="form-control" id="field" maxlength="2"  max="10">
-	    <label>Andar/Bloco:</label>
+	    <label>Andar/Bloco*</label>
 	    <select name="sala_andar" class="form-control">
             <option disabled>Escolha o bloco onde a sala está localizada</option> 
             <option disabled>---</option> 
