fc9ffd966b313b2002bc24edd0b4edb809a263c2
Erickson <erickson.rinho@hotmail.com>
2019-06-06 05:25:18 -0300

    Padronizando as Views de Tipos de Problemas

--- resources/views/tipos_problemas/tiposProblemas.blade.php
+++ resources/views/tipos_problemas/tiposProblemas.blade.php
@@ -14,7 +14,6 @@
             <h1 id="titulo">Tipos de Problemas</h1>
             <br>
         </div>
-        <br>
         <table class="table table-striped table-bordered" id="example">
             <thead class="thead-light">
                 <tr align="center">
@@ -66,20 +65,70 @@
 
         <script> 
             $(document).ready(function (){
-                @yield('datatables')
                 $('#vis-menu').click();
                 $('#visu-tipo-problemas').parent('li').addClass("active");
-            });                
+                $('#example').DataTable({ 
+                    oLanguage:{
+                        sProcessing: "Processando...",
+                        sLengthMenu: "Mostar _MENU_ registros pro página",
+                        sZeroRecords: "Nada encontrado com esse critérios",
+                        sEmptyTable: "Não há dados para serem mostrados",
+                        sLoadingRecords: "Carregando...",
+                        sInfo: "Mostrando _START_ a _END_ de _TOTAL_ registros",
+                        sInfoEmpty: "Mostrando 0 até 0 de 0 registros",
+                        sInfoFiltered: "(filtro aplicado em _MAX_ registros)",
+                        sInfoPostFix: "",
+                        sInfoThousands: ".",
+                        sSearch: "Pesquisar:",
+                        sUrl: "",
+                            oPaginate:{
+                                sFirst: "Primeira",
+                                sPrevious: "Anterior",
+                                sNext: "Próxima",
+                                sLast: "Última",
+                            },
+                        },
+                    bPaginate: false, //Next and Previous embaixo da tabela
+                    bLengthChange: false,  //Show and entries em cima da tabela
+                    bFilter: true, //Search em cima da tabela
+                    bInfo: false,  //Showing em baixo da tabela);
+                });          
+            });  
         </script>
-
 @endsection
 @section('ajax-js')
-    
-    <script type="text/javascript">
-        $(document).ready( function (){
-            @yield('datatables');
-        });
-    </script>
+        <script> 
+            $(document).ready(function (){
+                $('#vis-menu').click();
+                $('#visu-tipo-problemas').parent('li').addClass("active");
+                $('#example').DataTable({ 
+                    oLanguage:{
+                        sProcessing: "Processando...",
+                        sLengthMenu: "Mostar _MENU_ registros pro página",
+                        sZeroRecords: "Nada encontrado com esse critérios",
+                        sEmptyTable: "Não há dados para serem mostrados",
+                        sLoadingRecords: "Carregando...",
+                        sInfo: "Mostrando _START_ a _END_ de _TOTAL_ registros",
+                        sInfoEmpty: "Mostrando 0 até 0 de 0 registros",
+                        sInfoFiltered: "(filtro aplicado em _MAX_ registros)",
+                        sInfoPostFix: "",
+                        sInfoThousands: ".",
+                        sSearch: "Pesquisar:",
+                        sUrl: "",
+                            oPaginate:{
+                                sFirst: "Primeira",
+                                sPrevious: "Anterior",
+                                sNext: "Próxima",
+                                sLast: "Última",
+                            },
+                        },
+                    bPaginate: false, //Next and Previous embaixo da tabela
+                    bLengthChange: false,  //Show and entries em cima da tabela
+                    bFilter: true, //Search em cima da tabela
+                    bInfo: false,  //Showing em baixo da tabela);
+                }); 
+            });                
+        </script>
 @endsection
 
 @extends( (!$ajax) ? 'layouts.app' : 'layouts.ajax')
