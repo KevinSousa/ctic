4b12d7af525dfd3267d6c2a852f5a612ca22e9ca
Erickso <erickson.rinho@hotmail.com>
2019-06-12 00:53:59 -0300

    #88 - Correcao nas mensagens de Tipos de Problemas

--- resources/views/tipos_problemas/card-tiposProblemas.blade.php
+++ resources/views/tipos_problemas/card-tiposProblemas.blade.php
@@ -4,5 +4,10 @@
 	    <p style="margin-left:1em;color:red;font-size:small">{{$errors->first('probl_tipo')}}</p>
 	@endif 
 	<input type="text" name="probl_tipo" id="probl_tipo" value="{{ old('probl_tipo', $tipoProblema->probl_tipo ?? '') }}" required="" placeholder="Ex: Hd" class="form-control">
+	@if ($errors->has('probl_tipo')) 
+        <script >
+            $('#probl_tipo').addClass('alert-danger');                                            
+        </script>
+    @endif
 	<br>
 </div>
