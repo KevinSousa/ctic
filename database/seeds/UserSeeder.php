47d3b4a4b2dcd55296029482e44e69c37343bb17
Erickso <erickson.rinho@hotmail.com>
2019-06-15 12:04:18 -0300

    Correção na Seeder de Usuario

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -26,16 +26,49 @@ class UsersSeeder extends Seeder
         'user_funcao'   => '1',
         ]);        
         DB::table('users')->insert([
-        'user_name' => 'Usuario',
-        'user_cpf' => '22222222222',
-        'user_telefone' => '888888888',
-        'user_siap_matricula' => 'bbb123',
+        'user_name' => 'Professor',
+        'user_cpf' => '55555555555',
+        'user_telefone' => '3333333333',
+        'user_siap_matricula' => 'eee123',
         'user_imagem' => 'avatar-01.jpg',
-        'user_email' => 'user@ctic.ifpe',
+        'user_email' => 'professor@ctic.ifpe',
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
         'user_funcao'   => '2',
         ]);
+        DB::table('users')->insert([
+        'user_name' => 'Aluno',
+        'user_cpf' => '444444444444',
+        'user_telefone' => '666666666',
+        'user_siap_matricula' => 'ddd123',
+        'user_imagem' => 'avatar-01.jpg',
+        'user_email' => 'aluno@ctic.ifpe',
+        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'remember_token' => str_random(10),
+        'user_funcao'   => '3',
+        ]);
+        DB::table('users')->insert([
+        'user_name' => 'Terceirizado',
+        'user_cpf' => '33333333333',
+        'user_telefone' => '777777777',
+        'user_siap_matricula' => 'ccc123',
+        'user_imagem' => 'avatar-01.jpg',
+        'user_email' => 'terceirizado@ctic.ifpe',
+        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'remember_token' => str_random(10),
+        'user_funcao'   => '4',
+        ]);        
+        DB::table('users')->insert([
+        'user_name' => 'Administrativo',
+        'user_cpf' => '22222222222',
+        'user_telefone' => '888888888',
+        'user_siap_matricula' => 'bbb123',
+        'user_imagem' => 'avatar-01.jpg',
+        'user_email' => 'administrativo@ctic.ifpe',
+        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'remember_token' => str_random(10),
+        'user_funcao'   => '5',
+        ]);       
     }
     
 }
