2d3be7287f73785dc39ad01046650978339faa61
Erickso <erickson.rinho@hotmail.com>
2019-06-16 22:08:27 -0300

    Adicionando apenas nomes verdadeiros nos usuarios

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -12,10 +12,11 @@ $factory->define(App\User::class, function (Faker $faker) {
     $numMat     = $faker->numberBetween($min = 1000, $max = 9999);
     $letter1  = $faker->randomLetter;
     $letter2  = $faker->randomLetter;
-    $Mat  = $letter1.$letter2.$numMat; 
+    $Mat  = $letter1.$letter2.$numMat;
+    $pessoas = ['Miguel','Sophia','Davi','Alice','Arthur','Julia','Pedro','Isabella','Gabriel','Manuela','Bernardo' ,'Laura','Lucas','Luiza','Matheus'  ,'Valentina','Rafael','Giovanna','Heitor','Maria','Enzo','Helena','Guilherme','Beatriz','Nicolas','Lorenzo'];
    
     return [
-        'user_name' => $faker->name,
+        'user_name' => $faker->randomElement($pessoas),
         'user_cpf' => $cpf,
         'user_telefone' => $faker->phoneNumber,
         'user_siap_matricula' => $Mat,
